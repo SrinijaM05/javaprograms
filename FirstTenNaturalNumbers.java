4fd77c8c260329462f371399bcbbebf27f12e7ff
SrinijaM05 <127390184+SrinijaM05@users.noreply.github.com>
2023-03-09 15:38:00 +0530

    Update FirstTenNaturalNumbers.java

--- FirstTenNaturalNumbers.java
+++ FirstTenNaturalNumbers.java
@@ -1,3 +1,4 @@
+//write a java program to display first ten natural numbers
 public class FirstTenNaturalNumbers {
     public static void main(String[] args) {
         int n = 10; // number of natural numbers to display
@@ -6,3 +7,14 @@ public class FirstTenNaturalNumbers {
         }
     }
 }
+C:\Users\B-GST-CSE-L-PC22\Desktop\javaprogram>java FirstTenNaturalNumbers
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
