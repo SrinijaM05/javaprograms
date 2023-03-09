8b1e69b9b4260255252d5b73cba27f4b0a5ea78a
SrinijaM05 <127390184+SrinijaM05@users.noreply.github.com>
2023-03-09 15:37:18 +0530

    FirstTenNaturalNumbers

---
+++ FirstTenNaturalNumbers.java
@@ -0,0 +1,8 @@
+public class FirstTenNaturalNumbers {
+    public static void main(String[] args) {
+        int n = 10; // number of natural numbers to display
+        for (int i = 1; i <= n; i++) {
+            System.out.println(i);
+        }
+    }
+}
