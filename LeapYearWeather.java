e0fc8d067cf9fbc7ae08095cda12713a5b75967a
SrinijaM05 <127390184+SrinijaM05@users.noreply.github.com>
2023-03-09 15:23:58 +0530

    LeapYearWeather

---
+++ LeapYearWeather.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class LeapYearWeather {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        // Prompt the user to enter a year
+        System.out.print("Enter a year: ");
+        int year = input.nextInt();
+
+        // Check if the year is a leap year
+        boolean isLeapYear = false;
+        if (year % 4 == 0) {
+            if (year % 100 == 0) {
+                if (year % 400 == 0) {
+                    isLeapYear = true;
+                }
+            } else {
+                isLeapYear = true;
+            }
+        }
+
+        // Print out the result
+        if (isLeapYear) {
+            System.out.println(year + " is a leap year.");
+        } else {
+            System.out.println(year + " is not a leap year.");
+        }
+
+        // Print out a message about the weather
+        if (year >= 2023) {
+            System.out.println("The weather in " + year + " will be similar to the current weather.");
+        } else {
+            System.out.println("The weather in " + year + " was similar to the weather in 2022.");
+        }
+    }
+}
