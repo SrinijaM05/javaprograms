ec657bb234f8a591182c9dfbfcc1469c52ccfe93
SrinijaM05 <127390184+SrinijaM05@users.noreply.github.com>
2023-03-09 15:42:41 +0530

    SumAverage

---
+++ SumAverage.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class SumAverage {
+    public static void main(String[] args) {
+        int sum = 0;
+        double average;
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Please enter five numbers:");
+
+        for (int i = 0; i < 5; i++) {
+            int num = input.nextInt();
+            sum += num;
+        }
+
+        average = (double) sum / 5;
+
+        System.out.println("The sum of the five numbers is: " + sum);
+        System.out.println("The average of the five numbers is: " + average);
+    }
+}
